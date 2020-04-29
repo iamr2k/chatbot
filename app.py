dc8c916d2528a8950c3d193db78d4c41b3b9f7cc
Rahul Babu K <rahulbabu101@gmail.com>
2020-04-30 00:06:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-﻿from flask import Flask,render_template,url_for,request
+from flask import Flask,render_template,url_for,request
 import pandas as pd 
 import pickle
 import string
@@ -6,11 +6,34 @@ import re
 import pandas as pd
 import numpy as np
 
+
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM
+from keras.optimizers import Adam
+from keras import models as models
+
 import requests
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-processed = pd.read_csv("data.csv")
 
+question = pd.read_csv("question.csv")
+asindf = pd.read_csv("asindf.csv")
+answer = pd.read_csv("answer.csv")
+features pd.read_csv("features.csv")
+
+
+
+
+def clean(x):
+    x = re.sub(r'http\S+', '', x))
+    x = re.sub(r'[^\w]', ' ', x).lower())
+    x = re.sub(r'(?:^| )\w(?:$| )', ' ', x).strip())
+    x = x.replace('  ',' ')
+
+    return x
 
 app = Flask(__name__)
 
@@ -24,49 +47,75 @@ def predict():
    
     if request.method == 'POST':
 
-        message = request.form.get('message')
-                
-        
+        user_input_question = request.form.get('message')
 
-        with open('matrix.pkl', 'rb') as f:
-            tfidf_matrix = pickle.load(f)
-        with open('vector.pkl', 'rb') as f:
+        question0 = [[0]]
+        question0[0][0] = user_input_question
+        question = np.array(question0)
+        data = pd.DataFrame(question0)
+        data["cleaned"] = question0
+
+        texts_test1 = data.cleaned.astype(str)
+        model1 = models.load_model('/content/faq/lstm.h5')
+        with open('/content/faq/dnntokenizer.pkl', 'rb') as f:
+            tokenizer = pickle.load(f)
+
+        sequences_test1 = tokenizer.texts_to_sequences(texts_test1)
+
+        word_index = tokenizer.word_index
+        x_test1 = pad_sequences(sequences_test1, maxlen=20)
+        pred31 = model1.predict(x_test1)
+        if (pred31 >0.5) == True : 
+            pred32 = "yes/no"
+        else :
+            pred32 = "open-ended"
+        print("prediction = ",pred32)
+
+        with open('/content/faq/asindftfidfvector.pkl', 'rb') as f:
             tfidf_vectorizer = pickle.load(f)
+        with open('/content/faq/asindftfidf.pkl', 'rb') as f:
+            tfidf_matrix = pickle.load(f)
 
+        question= question0[0][0]
 
-        result1 = []
-        result2 = []
-        result3 = []
-
-        message = message
-       
-        query_vect = tfidf_vectorizer.transform([message])
+        query_vect = tfidf_vectorizer.transform([question])
             
         similarity = cosine_similarity(query_vect, tfidf_matrix)
         top_5_simmi = similarity[0].argsort()[-5:][::-1]
-       
-  
-        for i in top_5_simmi:
-            try:
-                result1.append(processed.iloc[i]['question'])   
-            except:
-                result1.append("na") 
-            try:  
-                result2.append(similarity[0, i])
-            except:
-                result2.append("na")
-            try:
-                result3.append(processed.iloc[i]['answer'])
-            except:
-                result3.append("na")
-        print("***************************",result1)
-            
 
+        result = []
+        for i in top_5_simmi:
+            result.append(asindf.iloc[i]['asin'])      
+            #print("top questions", processed[processed.asin == asindf.iloc[i]['asin'] ].question)
+            print('similarity: {:.2%}'.format(similarity[0, i]))
+        print(result)
+
+        question = question + " " +str(result[0]) + " " +pred32
+
+        with open('/content/faq/qtfidfvector.pkl', 'rb') as f:
+            tfidf_vectorizer = pickle.load(f)
+        with open('/content/faq/questiontfidf.pkl', 'rb') as f:
+            tfidf_matrix = pickle.load(f)
+
+        query_vect = tfidf_vectorizer.transform([question])
+        similarity = cosine_similarity(query_vect, tfidf_matrix)
+        top_5_simmi = similarity[0].argsort()[-15:][::-1]
+
+        result0 = []
+        result1 = []
+        result2 = []
+
+
+        for i in top_5_simmi:
+            result0.append(questiondf.iloc[i]['question'])
+            result1.append(answerdf.iloc[i]['answer'])       
+            #print("top questions", processed[processed.asin == asindf.iloc[i]['asin'] ].question)
+            result2.append(similarity[0, i])
 
 
         
 
-    return render_template('result.html', result1=result1,result2=result2,result3=result3)
+    return render_template('result.html', result1=result0,result2=result2,resul3=result1)
 
 
 
