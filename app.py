dc18ae35698a4b0770136e53e39e8c6103b93ce0
Rahul Babu K <rahulbabu101@gmail.com>
2020-04-30 09:00:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,70 +47,66 @@ def predict():
    
     if request.method == 'POST':
 
-        user_input_question = request.form.get('message')
+       user_input_question = request.form.get('message')
 
-        question0 = [[0]]
-        question0[0][0] = user_input_question
-        question = np.array(question0)
-        data = pd.DataFrame(question0)
-        data["cleaned"] = question0
+       question0 = [[0]]
+       question0[0][0] = user_input_question
+       question = np.array(question0)
+       data = pd.DataFrame(question0)
+       data["cleaned"] = question0
 
-        texts_test1 = data.cleaned.astype(str)
-        model1 = models.load_model('/content/faq/lstm.h5')
-        with open('/content/faq/dnntokenizer.pkl', 'rb') as f:
-            tokenizer = pickle.load(f)
+       texts_test1 = data.cleaned.astype(str)
+       model1 = models.load_model('/content/faq/lstm.h5')
+       with open('/content/faq/dnntokenizer.pkl', 'rb') as f:
+           tokenizer = pickle.load(f)
 
-        sequences_test1 = tokenizer.texts_to_sequences(texts_test1)
+       sequences_test1 = tokenizer.texts_to_sequences(texts_test1)
 
-        word_index = tokenizer.word_index
-        x_test1 = pad_sequences(sequences_test1, maxlen=20)
-        pred31 = model1.predict(x_test1)
-        if (pred31 >0.5) == True : 
-            pred32 = "yes/no"
-        else :
-            pred32 = "open-ended"
-        print("prediction = ",pred32)
+       word_index = tokenizer.word_index
+       x_test1 = pad_sequences(sequences_test1, maxlen=20)
+       pred31 = model1.predict(x_test1)
+       if (pred31 >0.5) == True : 
+           pred32 = "yes/no"
+       else :
+           pred32 = "open-ended"
 
-        with open('/content/faq/asindftfidfvector.pkl', 'rb') as f:
-            tfidf_vectorizer = pickle.load(f)
-        with open('/content/faq/asindftfidf.pkl', 'rb') as f:
-            tfidf_matrix = pickle.load(f)
+       with open('/content/faq/asindftfidfvector.pkl', 'rb') as f:
+           tfidf_vectorizer = pickle.load(f)
+       with open('/content/faq/asindftfidf.pkl', 'rb') as f:
+           tfidf_matrix = pickle.load(f)
 
-        question= question0[0][0]
+       question= question0[0][0]
 
-        query_vect = tfidf_vectorizer.transform([question])
+       query_vect = tfidf_vectorizer.transform([question])
             
-        similarity = cosine_similarity(query_vect, tfidf_matrix)
-        top_5_simmi = similarity[0].argsort()[-5:][::-1]
+       similarity = cosine_similarity(query_vect, tfidf_matrix)
+       top_5_simmi = similarity[0].argsort()[-5:][::-1]
 
-        result = []
-        for i in top_5_simmi:
-            result.append(asindf.iloc[i]['asin'])      
-            #print("top questions", processed[processed.asin == asindf.iloc[i]['asin'] ].question)
-            print('similarity: {:.2%}'.format(similarity[0, i]))
-        print(result)
+       result = []
+       for i in top_5_simmi:
+           result.append(asindf.iloc[i]['asin'])
 
-        question = question + " " +str(result[0]) + " " +pred32
+       question = question + " " +str(result[0]) + " " +pred32
 
-        with open('/content/faq/qtfidfvector.pkl', 'rb') as f:
-            tfidf_vectorizer = pickle.load(f)
-        with open('/content/faq/questiontfidf.pkl', 'rb') as f:
-            tfidf_matrix = pickle.load(f)
+       with open('/content/faq/qtfidfvector.pkl', 'rb') as f:
+           tfidf_vectorizer = pickle.load(f)
+       with open('/content/faq/questiontfidf.pkl', 'rb') as f:
+           tfidf_matrix = pickle.load(f)
 
-        query_vect = tfidf_vectorizer.transform([question])
-        similarity = cosine_similarity(query_vect, tfidf_matrix)
-        top_5_simmi = similarity[0].argsort()[-15:][::-1]
+       query_vect = tfidf_vectorizer.transform([question])
+       similarity = cosine_similarity(query_vect, tfidf_matrix)
+       top_5_simmi = similarity[0].argsort()[-15:][::-1]
 
-        result0 = []
-        result1 = []
-        result2 = []
+       result0 = []
+       result1 = []
+       result2 = []
 
 
-        for i in top_5_simmi:
-            result0.append(questiondf.iloc[i]['question'])
-            result1.append(answerdf.iloc[i]['answer'])       
-            #print("top questions", processed[processed.asin == asindf.iloc[i]['asin'] ].question)
-            result2.append(similarity[0, i])
+       for i in top_5_simmi:
+           result0.append(questiondf.iloc[i]['question'])
+           result1.append(answerdf.iloc[i]['answer'])       
+           #print("top questions", processed[processed.asin == asindf.iloc[i]['asin'] ].question)
+           result2.append(similarity[0, i])
 
 
         
