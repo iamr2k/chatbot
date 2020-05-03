290f46abcf15e27ce9c5584ed679bf30f826b76b
Rahul Babu K <rahulbabu101@gmail.com>
2020-05-03 23:04:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ import numpy as np
 
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans 
 from keras.models import Sequential
 from keras.layers import Dense, Embedding, LSTM
 from keras.optimizers import Adam
