70cde6e2243317a0ef340709a9e3d5d7b256ea1b
Rahul Babu K <rahulbabu101@gmail.com>
2020-05-03 22:46:11 +0530

    update

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Embedding, LSTM
 from keras.optimizers import Adam
 from keras import models as models
-
+from sklearn.preprocessing import normalize
 import requests
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -22,7 +22,10 @@ from sklearn.metrics.pairwise import cosine_similarity
 questiondf = pd.read_csv("question.csv")
 asindf = pd.read_csv("asindf.csv")
 answerdf = pd.read_csv("answer.csv")
-features = pd.read_csv("feature.csv")
+
+productcode = []
+questtype = []
+clusterid = []
 
 
 
@@ -44,98 +47,98 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     
-   
     if request.method == 'POST':
+        user_input_question = clean(request.form.get('message'))
+        question0 = [[0]]
+        question0[0][0] = user_input_question
+        question = np.array(question0)
+        data = pd.DataFrame(question0)
+        data["cleaned"] = question0
+ 
+        texts_test1 = data.cleaned.astype(str)
+        model1 = models.load_model('lstm.h5')
+        with open('dnntokenizer.pkl', 'rb') as f:
+            tokenizer = pickle.load(f)
+ 
+ 
+        sequences_test1 = tokenizer.texts_to_sequences(texts_test1)
+        x_test1 = pad_sequences(sequences_test1, maxlen=20)
+        pred31 = model1.predict(x_test1)
+        if (pred31 >0.5) == True : 
+            pred32 = "yes/no"
+        else :
+            pred32 = "open-ended"
 
-       user_input_question = request.form.get('message')
+        print('**************** QuestionType = ',pred32)
+        questtype.append(pred32)
 
-       question0 = [[0]]
-       question0[0][0] = user_input_question
-       question = np.array(question0)
-       data = pd.DataFrame(question0)
-       data["cleaned"] = question0
+        with open('/content/pca.pkl', 'rb') as f :
+            pca = pickle.load(f)
+        with open('/content/kmeans.pkl', 'rb') as f :
+            kmeans = pickle.load(f)
+        with open('/content/tfidf.pkl', 'rb') as f :
+            tfidf = pickle.load(f)
+ 
+        def cluster (userinput):
+             message = [[0]]
+             message[0][0] = userinput
+             message = np.array(message)
+             data = pd.DataFrame(message)
+             data["content"] = message
+             tf_idf = tfidf.transform(data["content"])
+             tf_idf_norm = normalize(tf_idf)
+             tf_idf_array = tf_idf_norm.toarray()
+             Y_sklearn = pca.transform(tf_idf_array)
+             prediction = kmeans.predict(Y_sklearn)
+             
+             return userinput+' cluster'+str(prediction[0])
+         
+        with open('asindftfidfvector.pkl', 'rb') as f:
+            tfidf_vectorizer = pickle.load(f)
+        with open('asindftfidf.pkl', 'rb') as f:
+            tfidf_matrix = pickle.load(f)
+ 
+        question= question0[0][0]
+        clusterresult = cluster(question)
+        print("****************",clusterresult)
+        clusterid.append(clusterresult)
 
-
-
-       texts_test1 = data.cleaned.astype(str)
-       model1 = models.load_model('lstm.h5')
-       with open('dnntokenizer.pkl', 'rb') as f:
-           tokenizer = pickle.load(f)
-
-
-       sequences_test1 = tokenizer.texts_to_sequences(texts_test1)
-
-       word_index = tokenizer.word_index
-       x_test1 = pad_sequences(sequences_test1, maxlen=20)
-       pred31 = model1.predict(x_test1)
-       if (pred31 >0.5) == True : 
-           pred32 = "yes/no"
-       else :
-           pred32 = "open-ended"
-       with open('/content/pca.pkl', 'rb') as f :
-           pca = pickle.load(f)
-       with open('/content/kmeans.pkl', 'rb') as f :
-           kmeans = pickle.load(f)
-       with open('/content/tfidf.pkl', 'rb') as f :
-           tfidf = pickle.load(f)
-
-       def cluster (userinput):
-            message = [[0]]
-            message[0][0] = userinput
-            message = np.array(message)
-            data = pd.DataFrame(message)
-            data["content"] = message
-            tf_idf = tfidf.transform(data["content"])
-            tf_idf_norm = normalize(tf_idf)
-            tf_idf_array = tf_idf_norm.toarray()
-            Y_sklearn = pca.transform(tf_idf_array)
-            prediction = kmeans.predict(Y_sklearn)
-            
-            return userinput+' cluster'+str(prediction[0])
-        
-       with open('asindftfidfvector.pkl', 'rb') as f:
-           tfidf_vectorizer = pickle.load(f)
-       with open('asindftfidf.pkl', 'rb') as f:
-           tfidf_matrix = pickle.load(f)
-
-       question= question0[0][0]
-       clusterresult = cluster(question)
-
-       query_vect = tfidf_vectorizer.transform([question])
-            
-       similarity = cosine_similarity(query_vect, tfidf_matrix)
-       top_5_simmi = similarity[0].argsort()[-5:][::-1]
-
-       result = []
-       for i in top_5_simmi:
-           result.append(asindf.iloc[i]['asin'])
-
-       question = question + " " +str(result[0]) + " " +pred32 + " " +clusterresult
-
-       with open('qtfidfvector.pkl', 'rb') as f:
-           tfidf_vectorizer = pickle.load(f)
-       with open('questiontfidf.pkl', 'rb') as f:
-           tfidf_matrix = pickle.load(f)
-
-       query_vect = tfidf_vectorizer.transform([question])
-       similarity = cosine_similarity(query_vect, tfidf_matrix)
-       top_5_simmi = similarity[0].argsort()[-15:][::-1]
-
-       result0 = []
-       result1 = []
-       result2 = []
-
-
-       for i in top_5_simmi:
-           result0.append(questiondf.iloc[i]['question'])
-           result1.append(answerdf.iloc[i]['answer'])       
-           result2.append(similarity[0, i])
-
-
-        
-
+ 
+        query_vect = tfidf_vectorizer.transform([question])
+             
+        similarity = cosine_similarity(query_vect, tfidf_matrix)
+        top_5_simmi = similarity[0].argsort()[-5:][::-1]
+ 
+        result = []
+        for i in top_5_simmi:
+            result.append(asindf.iloc[i]['asin'])
+        print('*************** Product code (asin)',result[0])
+        question = question + " " +str(result[0]) + " " +pred32 + " " +clusterresult
+ 
+        with open('qtfidfvector.pkl', 'rb') as f:
+            tfidf_vectorizer = pickle.load(f)
+        with open('questiontfidf.pkl', 'rb') as f:
+            tfidf_matrix = pickle.load(f)
+ 
+        query_vect = tfidf_vectorizer.transform([question])
+        similarity = cosine_similarity(query_vect, tfidf_matrix)
+        top_5_simmi = similarity[0].argsort()[-15:][::-1]
+ 
+        result0 = []
+        result1 = []
+        result2 = []
+ 
+ 
+        for i in top_5_simmi:
+            result0.append(questiondf.iloc[i]['question'])
+            result1.append(answerdf.iloc[i]['answer'])       
+            result2.append(similarity[0, i])
+ 
+ 
+         
+ 
     return render_template('result.html', result0=result0,result1=result1,result2=result2)
-
+ 
 
 
 if __name__ == '__main__':
